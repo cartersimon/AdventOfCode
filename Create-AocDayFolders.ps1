9841c55790eac9e1b475a8d4042a608d947e17b4
cartersimon <carter.simon@gmail.com>
2022-11-19 22:27:48 +0100

    downloaded input files for 1st 4 days

--- Create-AocDayFolders.ps1
+++ Create-AocDayFolders.ps1
@@ -1,12 +1,7 @@
 ﻿## Script to create daily folders for whole event
 
 # Create folders 01a/b -> 09a/b
-for ( $i = 1 ; $i -le 9 ; $i++ ) { 
-    mkdir .\"day0$($i)a"
-    mkdir .\"day0$($i)b"
-}
-# Create folders 10a/b -> 25a/b
-for ( $i = 10 ; $i -le 25 ; $i++ ) { 
+for ( $i = 1 ; $i -le 25 ; $i++ ) { 
     mkdir .\"day$($i)a"
     mkdir .\"day$($i)b"
 }
---
+++ Get-AocInputData.ps1
@@ -0,0 +1,47 @@
+﻿## Script to download input files
+
+## *** This will not work as it is, needs some way to pass logon creds or use browser cookie ***
+## Abandoning for now
+
+
+
+
+
+
+## Check script is running in correct folder
+# get current folder, not the whole path
+$curFolder = Split-Path -Path (Get-Location) -Leaf
+
+# assuming we're in the correct folder, remove "AOC" from the string leave just the year
+$year = $curFolder -replace '[AOC]',''
+
+# if year doesn't have 4 characters exit the script
+if ($year -notmatch '20\d\d' ) {
+    $errormsg = "`nYou don't seem to be in the year folder, please double check where the script is running."
+    if ($Host.Name -eq 'Windows PowerShell ISE Host') {
+        throw $errormsg
+    } else {
+        exit 1
+    }
+}
+
+    
+# Example source URI... https://adventofcode.com/2021/day/5/input
+
+# Get day number from user
+$day = [int](Read-Host -Prompt "Enter day number")
+
+# download the input file and put into correct folders (if it's day 4, needs to go into "day4a" & day4b)
+
+# Source file location
+#$source = "https://adventofcode.com/2021/day/$($day)/input"
+$source = "https://adventofcode.com/$($year)/day/$($day)/input"
+
+# Destination to save the file
+$destinationA = ".\day$($day)a\input.txt"
+$destinationB = ".\day$($day)b\input.txt"
+
+#Download the file
+Invoke-WebRequest -Uri $source -OutFile $destinationA
+Invoke-WebRequest -Uri $source -OutFile $destinationB
+
