696aad77c87bd22fb762088211ce2c61099cdee7
cartersimon <carter.simon@gmail.com>
2022-12-01 14:22:54 +0100

    folder restructure/update create-aocDayFolders.ps1

---
+++ AOC2021/day4/aoc2021-day4b.ps1
@@ -0,0 +1,15 @@
+## Advent of Code 2021 - Day 4 - Part 2
+
+# How to 
+#  represent boards
+#  highlight called numbers 
+#  store last number called
+#  go 1 board at a time? 
+
+# read in data file
+
+# Split drawn numbers from the boards
+
+# define locations a board can win
+
+# check boards to find winner
--- Create-AocDayFolders.ps1
+++ Create-AocDayFolders.ps1
@@ -7,20 +7,17 @@ $year = "22"
 
 for ( $i = 1 ; $i -le 25 ; $i++ ) { 
     # Create day folders
-    mkdir .\"day$($i)a"
-    mkdir .\"day$($i)b"
+    mkdir .\"day$($i)"
     
     # create empty files ready to put input data
-    New-Item .\day$($i)a\input.txt
-    New-Item .\day$($i)a\testinput.txt
-    New-Item .\day$($i)b\input.txt
-    New-Item .\day$($i)b\testinput.txt
+    New-Item .\day$($i)\input.txt
+    New-Item .\day$($i)\testinput.txt
     
     # create PS1 files ready to code, with identifying comment at top of file
-    New-Item .\day$($i)a\aoc$($year)-day$($i)a.ps1
-    Set-Content .\day$($i)a\aoc$($year)-day$($i)a.ps1 "## Advent of Code 20$($year) - Day $($i) - Part 1"
+    New-Item .\day$($i)\aoc$($year)-day$($i)a.ps1
+    Set-Content .\day$($i)\aoc$($year)-day$($i)a.ps1 "## Advent of Code 20$($year) - Day $($i) - Part 1"
 
-    New-Item .\day$($i)b\aoc$($year)-day$($i)b.ps1
-    Set-Content .\day$($i)b\aoc$($year)-day$($i)b.ps1 "## Advent of Code 20$($year) - Day $($i) - Part 2"
+    New-Item .\day$($i)\aoc$($year)-day$($i)b.ps1
+    Set-Content .\day$($i)\aoc$($year)-day$($i)b.ps1 "## Advent of Code 20$($year) - Day $($i) - Part 2"
 
 }
