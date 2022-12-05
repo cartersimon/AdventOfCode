e4617f74ef0f847f8ab801ae39364df05d7ee23d
cartersimon <carter.simon@gmail.com>
2022-12-05 16:36:02 +0100

    day 3, part 2, initial tinkering

--- AOC2022/day3/aoc22-day3b.ps1
+++ AOC2022/day3/aoc22-day3b.ps1
@@ -1,7 +1,7 @@
 ## Advent of Code 2022 - Day 3 - Part 2
 
 # Get input data
-$ContentsAllBags = Get-Content .\input.txt
+$ContentsAllBags = Get-Content .\testinput.txt
 
 # Dictionary to store item values
 $itemValues = [ordered]@{}
@@ -17,31 +17,50 @@ for ($value = 27 ; $value -le 52 ; $value++){
     $itemKey++
 }
 
-$total = 0
+#$total = 0
 
-# create groups of 3
+# Compare groups of 3
+for ( $i = 0 ; $i -le ($ContentsAllBags.Length - 3) ; $i += 3 ){
+    #foreach ($elfAitem in $ContentsAllBags[$i]){
+    for ( $j = 0 ; $j -le ($ContentsAllBags[$i]).Length ; $j++ ){
+        if ( $ContentsAllBags[$i+1] -ccontains ($ContentsAllBags[$i])[$j] ) {
+            Write-Host ($ContentsAllBags[$i])[$j]
+            Write-Host "...found in A and B"
+        }
+            #$elfAitem
+            # if ($ContentsAllBags[$i+1] -ccontains $elfAitem){
+            #     Write-Host "$elfAitem is in elfA & elfB"
+            #     if ($ContentsAllBags[$i+2] -ccontains $elfAitem){
+            #         Write-Host "$elfAitem is in all"
+            #         $total += $itemValues[$itemsA]
+            #         break
+            # }
+
+    }
+}
+
 
 # find common item
 
 
-# Split bag into 2 compartments
-foreach ( $bag in $ContentsAllBags){
-    $found = $false
-    $compartmentSize = $bag.Length/2
-    $compartmentA = $bag[0..($compartmentSize - 1)]
-    #Write-Host $compartmentA
+# # Split bag into 2 compartments
+# foreach ( $bag in $ContentsAllBags){
+#     $found = $false
+#     $compartmentSize = $bag.Length/2
+#     $compartmentA = $bag[0..($compartmentSize - 1)]
+#     #Write-Host $compartmentA
 
-    $compartmentB = $bag[$compartmentSize..($bag.Length - 1)]
-    #Write-Host $compartmentB
+#     $compartmentB = $bag[$compartmentSize..($bag.Length - 1)]
+#     #Write-Host $compartmentB
 
-    foreach ($itemA in $compartmentA){
-        if ($compartmentB -ccontains $itemA){
-            #write-host "$itemA is in both compartments of this bag"
-            $total += $itemValues[$itemA]
-            $found = $true
-        }
-        if ($found){ break }
-    }
-}
+#     foreach ($itemA in $compartmentA){
+#         if ($compartmentB -ccontains $itemA){
+#             #write-host "$itemA is in both compartments of this bag"
+#             $total += $itemValues[$itemA]
+#             $found = $true
+#         }
+#         if ($found){ break }
+#     }
+# }
 
-$total
+#$total
