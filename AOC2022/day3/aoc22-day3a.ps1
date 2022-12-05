e83c7df163dd7f41d9c89a878233d46466a07e5a
cartersimon <carter.simon@gmail.com>
2022-12-05 16:08:50 +0100

    day 3, part 1 complete

--- AOC2022/day3/aoc22-day3a.ps1
+++ AOC2022/day3/aoc22-day3a.ps1
@@ -1,1 +1,36 @@
 ## Advent of Code 2022 - Day 3 - Part 1
+
+# Get input data
+$ContentsAllBags = Get-Content .\input.txt
+
+# Dictionary to store item values
+$itemValues = [ordered]@{}
+$itemKey = 97
+for ( $value = 1 ; $value -le 26 ; $value++){
+    $itemValues.Add([char]$itemKey,$value)
+    $itemKey++
+}
+
+$itemKey = 65
+for ($value = 27 ; $value -le 52 ; $value++){
+    $itemValues.Add([char]$itemKey,$value)
+    $itemKey++
+}
+
+$total = 0
+# Split bag into 2 compartments
+foreach ( $bag in $ContentsAllBags){
+    $bagSize = $bag.Length
+    $compartmentSize = $bagSize/2
+    $compartmentA = $bag[0..($compartmentSize - 1)]
+    $compartmentB = $bag[$compartmentSize..($bagSize - 1)]
+
+    foreach ($itemA in $compartmentA){
+        if ($compartmentB -ccontains $itemA){
+            $total += $itemValues[$itemA]
+            break
+        }
+    }
+}
+
+$total
--- AOC2022/day3/aoc22-day3b.ps1
+++ AOC2022/day3/aoc22-day3b.ps1
@@ -1,1 +1,47 @@
 ## Advent of Code 2022 - Day 3 - Part 2
+
+# Get input data
+$ContentsAllBags = Get-Content .\input.txt
+
+# Dictionary to store item values
+$itemValues = [ordered]@{}
+$itemKey = 97
+for ( $value = 1 ; $value -le 26 ; $value++){
+    $itemValues.Add([char]$itemKey,$value)
+    $itemKey++
+}
+
+$itemKey = 65
+for ($value = 27 ; $value -le 52 ; $value++){
+    $itemValues.Add([char]$itemKey,$value)
+    $itemKey++
+}
+
+$total = 0
+
+# create groups of 3
+
+# find common item
+
+
+# Split bag into 2 compartments
+foreach ( $bag in $ContentsAllBags){
+    $found = $false
+    $compartmentSize = $bag.Length/2
+    $compartmentA = $bag[0..($compartmentSize - 1)]
+    #Write-Host $compartmentA
+
+    $compartmentB = $bag[$compartmentSize..($bag.Length - 1)]
+    #Write-Host $compartmentB
+
+    foreach ($itemA in $compartmentA){
+        if ($compartmentB -ccontains $itemA){
+            #write-host "$itemA is in both compartments of this bag"
+            $total += $itemValues[$itemA]
+            $found = $true
+        }
+        if ($found){ break }
+    }
+}
+
+$total
