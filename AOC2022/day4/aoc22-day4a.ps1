7dd3dcb2bc081b3046a3a5c9d31b3ffa5692ac93
cartersimon <carter.simon@gmail.com>
2022-12-07 17:52:59 +0100

    Day 4, part 1- complete

--- AOC2022/day4/aoc22-day4a.ps1
+++ AOC2022/day4/aoc22-day4a.ps1
@@ -1,1 +1,15 @@
 ## Advent of Code 2022 - Day 4 - Part 1
+
+$AllAssignments = (Get-Content .\input.txt).Split()
+
+$count = 0
+foreach ($pair in $AllAssignments){
+    $elf1, $elf2 = $pair.Split(",")
+    [int]$elf1min, [int]$elf1max = $elf1.Split("-")
+    [int]$elf2min, [int]$elf2max = $elf2.Split("-")
+    
+    if ( ($elf1min -ge $elf2min -and $elf1max -le $elf2max) -or ($elf2min -ge $elf1min -and $elf2max -le $elf1max) ){
+        $count++
+    }
+}
+$count
